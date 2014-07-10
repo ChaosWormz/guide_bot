3a836467ed0f7ca74f12c4e9bbc0a7eee56c8859
Craig Robbins <crobbins@localhost.localdomain>
2014-07-10 13:09:01 +1000

    Rewrite

--- init.lua
+++ init.lua
@@ -1,45 +1,61 @@
-minetest.register_on_chat_message(function(name, message)
-	local privs = minetest.get_player_privs(name)
-	if privs.bot then
-		if message == "~protect" then
-			minetest.chat_send_all("<BOT> To protect your area: dig down in one corner and type: '/area_pos1'. Then stack high in the opposite corner and type: '/area_pos2'")
-			minetest.chat_send_all("<BOT> Once you have set the area positions, type: '/protect (your area's name)' without quotes or parentheses. If this is too hard, ask a moderator or admin for help.")
-		end
+minetest.register_privilege("bot", {
+	description = "Use the guide bot.",
+	give_to_singleplayer= false,
+})
 
-		if message == "~respect" then
-			minetest.chat_send_all("<BOT> Respect other players. Threats of any kind are unwelcome. Discrimination is not tolerated. Be polite.")
-		end
+minetest.register_on_chat_message(function(player, message)
 
-		if message == "~skins" then
-			minetest.chat_send_all("<BOT> To get a custom skin you will need to provide a link to it and give it to an Admin, don't ask an Admin to pick a skin for you. Your Skin will be enabled after a server restart")
-		end
+	local pretext = ""
 
-		if message == "~dating" then
-			minetest.chat_send_all("<BOT> This server isn't a dating website, most of the players are kids.")
-		end
+	local responses = {
+		["~protect"] = 	{	"To protect your area: dig down in one corner and type: '/area_pos1'. " ..
+													"Then stack high in the opposite corner and type: '/area_pos2'",
+												"Once you have set the area positions, type: '/protect (your area's name)' without quotes or parentheses. " ..
+													"If this is too hard, ask a moderator or admin for help."
+		},
 
-		if message == "~minning" or message == "~interact" then
-			minetest.chat_send_all("<BOT> In order to be able to play you will first need to read the rules and then ask a member of staff to give you interact.")
-		end
+		["~respect"] = 	{ "Respect other players. Threats of any kind are unwelcome. " ..
+												"Discrimination is not tolerated. Be polite."
+		},
 
-		if message == "~English" or message == "~english" then
-			minetest.chat_send_all("<BOT> The majority of users on this server speak English. So that we can all communicate, please speak in English")
-		end
+		["~skins"] = 		{ "To get a custom skin you will need to provide a link to it and give it to an Admin. " ..
+												"Don't ask an Admin to pick a skin for you. Your skin will be enabled after a server " ..
+												"restart"
+		},
+
+		["~dating"] =		{ "This server is not a dating service. Using it as such will result in a ban." },
+
+		["~interact"] = { "In order to be able to play you will first need to read and agree with the rules, " ..
+												"and then ask a member of staff to give you interact."
+		},
+
+		["~english"] =	{ "The majority of players on this server speak English. " ..
+												"So that we can all communicate, please speak in English."
+		},
+
+		["~language"] = { "This is a family-friendly server: that means no swearing or profanity." },
+
+		["~extraprivs"] = { "Please don't ask for privileges like fly or no-clip or admin access. " ..
+													"You won't get them." },
+	}
+
+	if message == nil or message:sub(1, 1) ~= '~' then
+		return
+	end
+
+	if not minetest.get_player_privs(player).bot then
+		minetest.chat_send_player(player, "Insufficient privileges", true)
+		return
+	end
 	
-		if message == "~languge" then
-			minetest.chat_send_all("<BOT> This is a family-friendly server: that means no swearing or profanity.")
-		end
-	
-		if message == "~extraprivs" then
-			minetest.chat_send_all("<BOT> Please don't ask for privileges like fly or no-clip or admin access. you won't get them.")
-		end
-			return
+	lines = responses[string.lower(message)]
+	if not lines then
+		minetest.chat_send_player(player, "Unknown command", true)
+		return
+	end
+
+	for _, ln in ipairs(lines) do
+		minetest.chat_send_all(pretext .. ln)
 	end
 end)
 
---register priv
-
-minetest.register_privilege("bot", {
-	description = "use the shadowbot like bot.",
-	give_to_singleplayer= false,
-})
